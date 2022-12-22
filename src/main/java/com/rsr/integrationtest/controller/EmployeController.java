f111ca4ff8bf3569c50bb483cdd9bdeae437a90e
Ricardo Serrano <ricardo.ss@live.com>
2022-12-22 14:55:06 -0600

    feat: add GET by id logic

--- src/main/java/com/rsr/integrationtest/controller/EmployeController.java
+++ src/main/java/com/rsr/integrationtest/controller/EmployeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.rsr.integrationtest.domain.Employee;
 import com.rsr.integrationtest.service.EmployeeService;
 import com.rsr.integrationtest.service.EmployeeServiceImpl;
 
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -31,5 +33,10 @@ public class EmployeController {
     @GetMapping
     public ResponseEntity<List<Employee>> getAllEmployees(){
         return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> findEmployeeById(@PathVariable(name = "id") String id){
+        return new ResponseEntity<>(employeeService.getEmployeeById(id).get(), HttpStatus.OK);
     }
 }
